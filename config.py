15997ce99d31e2e128cf82e292ab2c08606f1e26
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-01-25 16:19:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5012922697:AAHgrUOL8gsoyvZO5IkVphGc3oOjglRKKjw"
+BOT_TOKEN = "5012922697:AAGYiGQQhgj7xfXIB_MB-6A7m7meY4CGUVY"
 APP_URL = "https://botuserpass.herokuapp.com/" + BOT_TOKEN
